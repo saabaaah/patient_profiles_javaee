0d980605a8f7b5995edff50ebec57ace7ef38396
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-08-23 15:02:25 +0100

    + CROS pb : @configurztion

--- first.app.ws/src/main/java/com/eprostam/first/app/ws/WebConfig.java
+++ first.app.ws/src/main/java/com/eprostam/first/app/ws/WebConfig.java
@@ -1,8 +1,10 @@
 package com.eprostam.first.app.ws;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer{
 
 	@Override
